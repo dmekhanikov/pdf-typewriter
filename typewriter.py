f408d0b3a0efcc41afdc5363d97a9392935e6c27
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-09-15 13:59:21 +0300

    take font config from YAML

--- typewriter.py
+++ typewriter.py
@@ -7,21 +7,18 @@ import yaml
 out_dir = 'pdf'
 text_yml_file = 'text.yml'
 
-point = 1
 inch = 72
-char_size = 12 * point
-spacing = 15 * point
 
 
-def make_canvas(output_filename):
+def make_canvas(output_filename, font_conf):
     c = canvas.Canvas(output_filename, pagesize=(8.5 * inch, 11 * inch))
     c.setStrokeColorRGB(0, 0, 0)
     c.setFillColorRGB(0, 0, 0)
-    c.setFont("Helvetica-Oblique", char_size)
+    c.setFont(font_conf['name'], font_conf['size'])
     return c
 
 
-def write_string(c, x, y, string):
+def write_string(c, x, y, string, spacing):
     for sym in string:
         c.drawString(x, y, sym)
         x += spacing
@@ -35,12 +32,15 @@ if __name__ == "__main__":
         os.makedirs(out_dir)
 
     text_pdf_file = out_dir + '/text.pdf'
-    c = make_canvas(text_pdf_file)
+
+    font_conf = text_desc['font']
+    c = make_canvas(text_pdf_file, font_conf)
+    spacing = font_conf['spacing']
 
     for snippet in text_desc['snippets']:
         x = snippet['x']
         y = snippet['y']
         text = snippet['text']
-        write_string(c, x, y, text)
+        write_string(c, x, y, text, font_conf['spacing'])
 
     c.save()
