bcb77c01987a8e3d9c49a24acd608b1e68432bfb
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-09-15 14:46:13 +0300

    print over another pdf

--- typewriter.py
+++ typewriter.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
-from reportlab.pdfgen import canvas
+import os
+from sys import argv
+
+import yaml
+from PyPDF2 import PdfFileReader, PdfFileWriter
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-
-from sys import argv
-import os
-import yaml
+from reportlab.pdfgen import canvas
 
 out_dir = 'pdf'
+text_pdf_file = out_dir + '/text.pdf'
+out_pdf_file = out_dir + '/output.pdf'
 
 inch = 72
 
@@ -31,22 +34,15 @@ def register_font(name, file):
     pdfmetrics.registerFont(TTFont(name, file))
 
 
-if __name__ == "__main__":
-    text_yml_file = argv[1]
-
-    with open(text_yml_file, 'r') as stream:
-        text_desc = yaml.safe_load(stream)
-
+def create_text_pdf(text_pdf_file, text_desc):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    text_pdf_file = out_dir + '/text.pdf'
-
     font_conf = text_desc['font']
-    
+
     if font_conf['file']:
         register_font(font_conf['name'], font_conf['file'])
-        
+
     c = make_canvas(text_pdf_file, font_conf)
     spacing = font_conf['spacing']
 
@@ -54,6 +50,37 @@ if __name__ == "__main__":
         x = snippet['x']
         y = snippet['y']
         text = snippet['text']
-        write_string(c, x, y, text, font_conf['spacing'])
+        write_string(c, x, y, text, spacing)
 
     c.save()
+
+
+def merge_pdfs(file1, file2, out_file):
+    input1 = PdfFileReader(file1)
+    input2 = PdfFileReader(file2)
+    output = PdfFileWriter()
+
+    for i, page in enumerate(input1.pages):
+        if i < input2.numPages:
+            page.mergePage(input2.getPage(i))
+        output.addPage(page)
+
+    output_stream = open(out_file, "wb")
+    output.write(output_stream)
+    output_stream.close()
+
+
+if __name__ == "__main__":
+    text_yml_file = argv[1]
+    input_pdf_file = argv[2] if len(argv) > 1 else None
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    with open(text_yml_file, 'r') as stream:
+        text_desc = yaml.safe_load(stream)
+
+    create_text_pdf(text_pdf_file, text_desc)
+
+    if input_pdf_file:
+        merge_pdfs(input_pdf_file, text_pdf_file, out_pdf_file)
