d3f11b73ef2f67d5d1f080d7c204e8e1e50cbcef
Denis Mekhanikov <dmekhanikov@gmail.com>
2019-09-15 13:37:55 +0300

    write text with constant spacing

--- typewriter.py
+++ typewriter.py
@@ -8,14 +8,22 @@ out_dir = 'pdf'
 
 point = 1
 inch = 72
+char_size = 12 * point
+spacing = 15 * point
 
 
 def make_canvas(output_filename):
     c = canvas.Canvas(output_filename, pagesize=(8.5 * inch, 11 * inch))
     c.setStrokeColorRGB(0, 0, 0)
     c.setFillColorRGB(0, 0, 0)
-    c.setFont("Helvetica-Oblique", 12 * point)
+    c.setFont("Helvetica-Oblique", char_size)
     return c
+
+
+def write_string(c, x, y, string):
+    for sym in string:
+        c.drawString(x, y, sym)
+        x += spacing
 
 
 if __name__ == "__main__":
@@ -24,5 +32,5 @@ if __name__ == "__main__":
 
     text_file = out_dir + '/text.pdf'
     c = make_canvas(text_file)
-    c.drawString(1 * inch, 10 * inch, 'Hello, World!')
+    write_string(c, 1 * inch, 10 * inch, 'Hello, World!')
     c.save()
